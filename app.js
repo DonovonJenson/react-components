0429e2f8f5bb0f99a834048d618b7f036704977d
Donovon <donovonjenson@hotmail.com>
2017-10-25 13:05:59 -0700

    complete react tutorial

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// TODO
+var GroceryList = (props) => (
+  <div> <ul> 
+  {props.items.map(item =>
+    <GroceryListItem item = {item}/>
+  )}
+  </ul></div>
+);
+
+class GroceryListItem extends React.Component{
+
+	constructor(props){
+	super(props);
+  this.state = {
+    done: false
+   };
+  }
+
+  onListItemClick(){
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+    render() {
+      var style = {
+        fontWeight: this.state.done ? 'bold' : 'normal'
+      }
+    	return(
+    	<li style={style} onMouseLeave={this.onListItemClick.bind(this)} onMouseEnter={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    	)
+    }
+}
+
+ReactDOM.render(<GroceryList items={['apple','carrot','pear']} />, document.getElementById("app"));
